73cfe2ad1999a7e76f4abe417fa19dad016834d3
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-07-21 12:13:10 +0530

    initial commint

---
+++ app.js
@@ -0,0 +1,9 @@
+var billInput = document.querySelector("#billAmount");
+var cashInput = document.querySelector("#cashAmount");
+var result =document.querySelector("#test");
+var btnCheck=document.querySelector("#check")
+
+var bill=billInput.value;
+var cash=cashInput.value;
+
+
