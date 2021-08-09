eb6e3a55c353c54e714cba0da293d6a93260aef3
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-08-09 10:49:55 +0530

    done

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ var outp=document.querySelector(".output")
 
 function clickNext(){
     var bill=bA.value;
+    var cDisp = document.querySelector(".cashDiv");
+    cDisp.style.display="block";
+    var tDisp = document.querySelector(".tableDiv");
+        tDisp.style.display="none";
+    outp.innerHTML=" ";
+    var resetValues = document.querySelectorAll(".reset");
+    console.log(resetValues)
+    for(let i=0;i<resetValues.length;i++){
+      resetValues[i].innerHTML="0"
+    }
+    
     
     return bill;
 }
@@ -17,59 +28,63 @@ function clickSubmit(){
     console.log(diff);
 
     if (diff == 0) {
-        
+      var tDisp = document.querySelector(".tableDiv");
+      tDisp.style.display="none";
         outp.innerHTML="No need Brother";
        
       }
       
-    if (diff > 0) {
-        
+    else if (diff > 0) {
+        var tDisp = document.querySelector(".tableDiv");
+        tDisp.style.display="block";
+        outp.innerHTML="";
         if (diff >= 2000) {
           var count = Math.floor(diff / 2000);
           diff = diff - 2000 * count;
-          var tw = document.querySelector("#twoThousand");
+          var tw = document.querySelector(".twoThousand");
           tw.innerHTML = count;
         }
         if (diff >= 500) {
           var count = Math.floor(diff / 500);
           diff = diff - 500 * count;
-          var tw = document.querySelector("#fiveHundred");
+          var tw = document.querySelector(".fiveHundred");
           tw.innerHTML = count;
         }
         if (diff >= 100) {
           var count = Math.floor(diff / 100);
           diff = diff - 100 * count;
-          var tw = document.querySelector("#hundred");
+          var tw = document.querySelector(".hundred");
           tw.innerHTML = count;
         }
         if (diff >= 20) {
           var count = Math.floor(diff / 20);
           diff = diff - 20 * count;
-          var tw = document.querySelector("#twenty");
+          var tw = document.querySelector(".twenty");
           tw.innerHTML = count;
         }
         if (diff >= 10) {
           var count = Math.floor(diff / 10);
           diff = diff - 10 * count;
-          var tw = document.querySelector("#ten");
+          var tw = document.querySelector(".ten");
           tw.innerHTML = count;
         }
         if (diff >= 5) {
           var count = Math.floor(diff / 5);
           diff = diff - 5 * count;
-          var tw = document.querySelector("#five");
+          var tw = document.querySelector(".five");
           tw.innerHTML = count;
         }
         if (diff >= 1) {
           var count = Math.floor(diff / 1);
           diff = diff - 1 * count;
-          var tw = document.querySelector("#one");
+          var tw = document.querySelector(".one");
           tw.innerHTML = count;
         }
       }
     
-      if (diff < 0) {
-        
+      else {
+        var tDisp = document.querySelector(".tableDiv");
+        tDisp.style.display="none";
         
         outp.innerHTML="Pay the Full Amount first please.";
         
